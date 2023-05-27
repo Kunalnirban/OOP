cd8b523883457b10d899891f7d98c7c168c4c808
Kunal Nirban <kunalnirban@mail.com>
2023-05-27 21:20:27 +0530

    Add files via upload

---
+++ Destructor.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef class test{
+    public :
+    test(){
+       cout<<"This is constructor"<<endl; 
+    }
+
+    ~test(){
+       cout<<"This is distructor"<<endl; 
+    }
+
+}t;
+
+int main()
+{
+    t ob;
+
+    return 0;
+}
